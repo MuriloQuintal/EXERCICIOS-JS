13e685c5bbd46a200d14f283aaa005d483be70d4
MuriloQuintal <130596582+MuriloQuintal@users.noreply.github.com>
2025-09-11 22:16:04 -0300

    Resolução do exercício 001

---
+++ Exercicio001/app.js
@@ -0,0 +1,6 @@
+let nome = "", idade = 0
+nome = prompt("Informe seu nome: ")
+idade = prompt("Informe sua Idade: ")
+alert("Olá, seja bem vindo(a) " + nome +" \nVocê tem " + idade + " anos")
+console.log("Olá, seja bem vindo(a) " + nome +" \nVocê tem " + idade + " anos")
+document.write("Olá, seja bem vindo(a) " + nome +" <br>Você tem " + idade + " anos")
