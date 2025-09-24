61dc5e6a9e5950233485c34f7af8ee06cdf806ed
MuriloQuintal <130596582+MuriloQuintal@users.noreply.github.com>
2025-09-23 22:20:02 -0300

    Exercício006

--- Exercicio007/app.js
+++ Exercicio007/app.js
@@ -1,4 +1,5 @@
 let dataAtual = new Date()
+// let diaDaSemana = dataAtual.getDate()
 console.log(dataAtual.getDay())
 if(dataAtual.getDay() == 0){
     document.getElementById("resposta").innerText = "Domingo"
@@ -15,4 +16,32 @@ if(dataAtual.getDay() == 0){
 }else if(dataAtual.getDay() == 6){
     document.getElementById("resposta").innerText = "Sábado"
 }else{ 
-    document.getElementById("resposta").innerText = ""}
+    document.getElementById("resposta").innerText = ""
+
+// switch (diaDaSemana) {
+//     case 0:
+//         document.getElementById("resposta").innerText = "Domingo"    
+//         break
+//     case 1:
+//         document.getElementById("resposta").innerText = "Segunda-feira"
+//         break
+//     case 2:
+//         document.getElementById("resposta").innerText = "Terça-feira"
+//         break    
+//     case 3:
+//         document.getElementById("resposta").innerText = "Quarta-feira"
+//         break
+//     case 4:
+//         document.getElementById("resposta").innerText = "Quinta-feira"
+//         break
+//     case 5:
+//         document.getElementById("resposta").innerText = "Sexta-feira"
+//         break
+//     case 6:
+//         document.getElementById("resposta").innerText = "Sabado"
+//         break
+//         default:
+//             document.getElementById("resposta").innerText = ""
+//         break;
+// }
+}
