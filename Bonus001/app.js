4a8e97c824d85ad443f8ace422734303e2fa5aec
MuriloQuintal <130596582+MuriloQuintal@users.noreply.github.com>
2025-09-26 22:28:02 -0300

    Bonus 001
    sons a partir de teclas

---
+++ Bonus001/app.js
@@ -0,0 +1,16 @@
+function fnTocarSom(tecla){
+    if(tecla == 65){
+        document.getElementById("som-65").currentTime = 0
+        document.getElementById("som-65").play()
+    }else if(tecla == 83){
+        document.getElementById("som-83").currentTime = 0
+        document.getElementById("som-83").play() 
+    }else if(tecla == 68){
+        document.getElementById("som-68").currentTime = 0
+        document.getElementById("som-68").play() 
+    }else if(tecla == 69){
+        document.getElementById("som-69").currentTime = 0
+        document.getElementById("som-69").play() 
+    }
+
+}
