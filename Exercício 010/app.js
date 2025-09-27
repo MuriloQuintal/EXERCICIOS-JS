dacb192992231168952fa5886e618d227602c7a2
MuriloQuintal <130596582+MuriloQuintal@users.noreply.github.com>
2025-09-26 21:36:40 -0300

    Exercicio 010
    Formulario esconde e aparece

--- Exercício 010/app.js
+++ Exercício 010/app.js
@@ -1,3 +1,26 @@
-function fnMediaAluno(){
-    console.log("Vaida certo")
+
+function fnCalcularMedia(){
+    let n1 = 0, n2 = 0, n3 = 0, n4 = 0, nome = "", media = 0
+    n1 = parseFloat(document.getElementById("n1").value)
+    n2 = parseFloat(document.getElementById("n2").value)
+    n3 = parseFloat(document.getElementById("n3").value)
+    n4 = parseFloat(document.getElementById("n4").value)
+    nome = document.getElementById("nome").value
+    media = (n1 + n2 + n3 + n4) / 4
+    document.getElementById("nome_resultado").innerHTML = nome
+    document.getElementById("media").innerHTML = media.toFixed(1)
+    fnExibir("resultado")
 }
+
+function fnExibir(painel_exibir){
+document.getElementById("painel_resultado").style.display="none"
+document.getElementById("painel_formulario").style.display="none"
+if(painel_exibir == "resultado"){
+    document.getElementById("painel_resultado").style.display="block"
+}else if (painel_exibir == "formulario"){
+    document.getElementById("painel_formulario").style.display="block"
+  
+}  
+}
+
+
