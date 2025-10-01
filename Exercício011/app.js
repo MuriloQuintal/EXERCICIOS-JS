9e7891619cac4c0bc266f51f600d4a88a78f8b66
MuriloQuintal <130596582+MuriloQuintal@users.noreply.github.com>
2025-09-30 21:09:24 -0300

    Exercício011
    Criado lista Array, para gravas os numeros, e calcular a media, contando os numeros e somando o valor dos numeros, depois dividindo um pelo outro

---
+++ Exercício011/app.js
@@ -0,0 +1,20 @@
+let listaDeNumeros = []
+function fnLimpar(){
+    document.getElementById("formulario").reset()
+}
+function fnSalvarNumero(){
+    let numero = parseFloat(document.getElementById("numero").value)
+    listaDeNumeros.push(numero)
+    console.log("funcionou? ",listaDeNumeros)
+}
+function fnCalcularMedia(){
+    let media = 0, qtdNumeros = 0, somaDosNumeros = 0, i = 0
+    qtdNumeros = listaDeNumeros.length
+    while(i<qtdNumeros){
+        somaDosNumeros += listaDeNumeros[i]
+        i++
+    }
+    media = somaDosNumeros / qtdNumeros
+    document.getElementById("media").innerHTML = media
+    listaDeNumeros = []
+}
