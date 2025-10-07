eac63480e4a110fed9d03f5f779adfbbccb34950
MuriloQuintal <130596582+MuriloQuintal@users.noreply.github.com>
2025-10-06 22:21:31 -0300

    Exercício013
    Exercicio 13 criando array, lista de placas e de carro e vagas, cadastrando nao criando. usando "for" e usando crase ao invés de aspas para enviar os dados em uma resposta

---
+++ Exercicio013/app.js
@@ -0,0 +1,32 @@
+let numeroDeVagas = []
+let veiculo= []
+function fnSalvarVeiculos(){
+    numeroDeVagas.push(document.getElementById("numero_vaga").value)
+   veiculo.push(document.getElementById("veiculo").value)
+      
+}
+
+function fnListarVeiculos(){
+    // console.dir(numeroDeVagas)
+    // console.dir(veiculo)
+    // document.getEle
+    
+    // let i = 0
+    // while(i < veiculo.length){
+    //     console.log(veiculo[i], numeroDeVagas[i])
+    //     i++
+    // }
+    
+    // for(declaração contador; condição; incremento){}
+    document.getElementById("tabela_veiculos").innerHTML=""
+    for(let i = 0; i < veiculo.length; i++){
+        document.getElementById("tabela_veiculos").innerHTML +=`<tr>`
+        document.getElementById("tabela_veiculos").innerHTML +=`<td> ${numeroDeVagas[i]}</td><td>${veiculo[i]}<td>`
+        document.getElementById("tabela_veiculos").innerHTML += `</tr>`
+    }
+
+
+
+}
+// function fnLimpar(){
+//     getElementById("numero_vaga")clea
