959941050375838726d90bb2ceda5b0f26b37bec
MuriloQuintal <130596582+MuriloQuintal@users.noreply.github.com>
2025-10-10 19:50:58 -0300

    Exercício014-B
    Inserido botao Revelar , sortear novo, revelardepois no botao revelar

--- Exercicio014-B/app.js
+++ Exercicio014-B/app.js
@@ -246,6 +246,17 @@ const pokemons = [
 
     return numeroSorteado
   }
-
+function fnEsconder(){
+  document.getElementById("numero").classList.add ("esconder-numero")
+  document.getElementById("nome").classList.add ("esconder-nome")
+  document.getElementById("foto").classList.add ("esconder-pokemon")
+  
+}
+function fnRevelar(){
+  document.getElementById("numero").classList.remove("esconder-numero")
+  document.getElementById("nome").classList.remove("esconder-nome")
+  document.getElementById("foto").classList.remove("esconder-pokemon")
+}
 
   fnListarPokemon()
+  fnEsconder()
